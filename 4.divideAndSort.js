2e5f3cec7310ab88d6d22163ead792ae50fe0e5a
Fajar-PNG29 <60532410+Fajar-PNG29@users.noreply.github.com>
2020-04-18 20:56:03 +0700

    Add files via upload

---
+++ 4.divideAndSort.js
@@ -0,0 +1,9 @@
+const divideAndSort = (num) =>{
+    	let tempr = num.toString().split(0);
+  		let divid = [];
+  		divid.push(tempr.sort())
+        let pisah = divid.toString().split(",").join('')
+		
+        console.log(pisah);
+}
+divideAndSort(5956560159466056);
